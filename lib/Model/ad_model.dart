19fcd7db86a55d43f962013da16a938a19a8ddf6
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-02-01 17:43:37 +0330

    Ad Model

---
+++ lib/Model/ad_model.dart
@@ -0,0 +1,15 @@
+class AdModel{
+  String title;
+  String image;
+  String address;
+  String price;
+  String count;
+  String category;
+  String callInfo;
+  String description;
+  String status;
+  String user;
+
+  AdModel(this.title, this.image, this.address, this.price, this.count,
+      this.category, this.callInfo, this.description, this.status, this.user);
+}
