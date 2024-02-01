b5e1b71a52d13bb78f73a3483a8fc9932a7fc23a
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-02-01 15:38:24 +0330

    ListView in Home Screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,6 +10,74 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("home"),);
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      itemCount: 3,
+      itemBuilder: (context, index) {
+        return Card(
+          margin: EdgeInsets.fromLTRB(16, 8, 16, 8),
+          elevation: 6,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(12))),
+          child: Container(
+            height: 170,
+            child: Row(
+              textDirection: TextDirection.rtl,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Container(
+                    color: Colors.amber,
+                    height: 130,
+                    width: 160,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(4),
+                      child: Stack(
+                        children: [
+                          Image.network(
+                            "https://www.uplooder.net/img/image/55/dc50aba858e6e91b89832ccf81e8edb2/laptop.jpg",
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 8),
+                      child: Container(
+                        color: Colors.red,
+                        height: 50,
+                        width: 175,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 8),
+                      child: Container(
+                        color: Colors.blue,
+                        height: 30,
+                        width: 175,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 34,
+                    ),
+                    Container(
+                      height: 30,
+                      width: 175,
+                      color: Colors.grey,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
