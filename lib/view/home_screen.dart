472564a9cc9e2f71d76b84f89d01270c6b94a173
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-02-05 19:24:53 +0330

    get ads from server

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:divar/Model/ad_model.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,158 +12,207 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<AdModel> listAds = [];
+
+  @override
+  void initState() {
+    fetchItems();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      physics: const BouncingScrollPhysics(),
-      scrollDirection: Axis.vertical,
-      itemCount: 3,
-      itemBuilder: (context, index) {
-        return Card(
-          margin: const EdgeInsets.fromLTRB(16, 8, 16, 8),
-          elevation: 6,
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(12))),
-          child: SizedBox(
-            height: 160,
-            child: Row(
-              textDirection: TextDirection.rtl,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Container(
-                    color: Colors.amber,
-                    height: 130,
-                    width: 160,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(4),
-                      child: Stack(
-                        children: [
-                          Image.network(
-                            "https://www.uplooder.net/img/image/55/dc50aba858e6e91b89832ccf81e8edb2/laptop.jpg",
+    return Scaffold(
+      body: listAds.length == 0
+          ? const Center(
+              child: Text(
+                "موردی برای نمایش یافت نشد",
+                style: TextStyle(
+                  fontFamily: 'irs',
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          : ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              scrollDirection: Axis.vertical,
+              itemCount: listAds.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+                  elevation: 6,
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12))),
+                  child: SizedBox(
+                    height: 160,
+                    child: Row(
+                      textDirection: TextDirection.rtl,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: SizedBox(
+                            height: 130,
                             width: 160,
-                            height: 170,
-                            fit: BoxFit.fill,
-                          ),
-                          Positioned(
-                            right: 0,
-                            bottom: 0,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: const BorderRadius.only(
-                                    bottomLeft: Radius.circular(10),
-                                    topLeft: Radius.circular(10)),
-                                color: Colors.black.withOpacity(0.5),
-                              ),
-                              child: const Padding(
-                                padding: EdgeInsets.symmetric(
-                                    vertical: 6, horizontal: 8),
-                                child: Row(
-                                  children: [
-                                    Text(
-                                      "7",
-                                      style: TextStyle(color: Colors.white),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(4),
+                              child: Stack(
+                                children: [
+                                  Image.network(
+                                    listAds[index].image,
+                                    width: 160,
+                                    height: 170,
+                                    fit: BoxFit.fill,
+                                    errorBuilder: (context, error, stackTrace) {
+                                      return const Center(child: Icon(Icons.image_not_supported_outlined,size: 40,color: Colors.grey,));
+                                    },
+                                  ),
+                                  Positioned(
+                                    right: 0,
+                                    bottom: 0,
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: const BorderRadius.only(
+                                            bottomLeft: Radius.circular(10),
+                                            topLeft: Radius.circular(10)),
+                                        color: Colors.black.withOpacity(0.5),
+                                      ),
+                                      child: const Padding(
+                                        padding: EdgeInsets.symmetric(
+                                            vertical: 6, horizontal: 8),
+                                        child: Row(
+                                          children: [
+                                            Text(
+                                              "7",
+                                              style:
+                                                  TextStyle(color: Colors.white),
+                                            ),
+                                            SizedBox(
+                                              width: 10,
+                                            ),
+                                            Icon(
+                                              Icons.camera_alt,
+                                              color: Colors.white,
+                                              size: 20,
+                                            ),
+                                          ],
+                                        ),
+                                      ),
                                     ),
-                                    SizedBox(
-                                      width: 10,
-                                    ),
-                                    Icon(
-                                      Icons.camera_alt,
-                                      color: Colors.white,
-                                      size: 20,
-                                    ),
-                                  ],
-                                ),
+                                  ),
+                                ],
                               ),
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                         Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 11),
+                              child: Text(
+                                listAds[index].title,
+                                textDirection: TextDirection.rtl,
+                                style: const TextStyle(
+                                    fontFamily: 'irs',
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            const Text(
+                              "در حد نو",
+                              textDirection: TextDirection.rtl,
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.black54,
+                                  fontFamily: 'irs'),
+                            ),
+                            const SizedBox(
+                              height: 3,
+                            ),
+                             Row(
+                              children: [
+                                Text(
+                                  "${listAds[index].price} ",
+                                  textDirection: TextDirection.rtl,
+                                  style: const TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.black54,
+                                      fontFamily: 'irs'),
+                                ),
+                                const Text(
+                                  "تومان",
+                                  textDirection: TextDirection.rtl,
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.black54,
+                                      fontFamily: 'irs'),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 3,
+                            ),
+                             Row(
+                              children: [
+                                const Text("لحظاتی پیش در",
+                                    style: TextStyle(
+                                        fontSize: 13,
+                                        color: Colors.black54,
+                                        fontFamily: 'irs')),
+                                const SizedBox(
+                                  width: 3,
+                                ),
+                                Text(listAds[index].address,
+                                    style: const TextStyle(
+                                        fontSize: 13,
+                                        color: Colors.black54,
+                                        fontFamily: 'irs')),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                const Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(top: 11),
-                      child: Text(
-                        "عنوان آگهی",
-                        textDirection: TextDirection.rtl,
-                        style: TextStyle(
-                            fontFamily: 'irs',
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Text(
-                      "در حد نو",
-                      textDirection: TextDirection.rtl,
-                      style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.black54,
-                          fontFamily: 'irs'),
-                    ),
-                    SizedBox(
-                      height: 3,
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          "120000 ",
-                          textDirection: TextDirection.rtl,
-                          style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.black54,
-                              fontFamily: 'irs'),
-                        ),
-                        Text(
-                          "تومان",
-                          textDirection: TextDirection.rtl,
-                          style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.black54,
-                              fontFamily: 'irs'),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 3,
-                    ),
-                    Row(
-                      children: [
-                        Text("لحظاتی پیش در",
-                            style: TextStyle(
-                                fontSize: 14,
-                                color: Colors.black54,
-                                fontFamily: 'irs')),
-                        SizedBox(
-                          width: 8,
-                        ),
-                        Text("مشهد",
-                            style: TextStyle(
-                                fontSize: 14,
-                                color: Colors.black54,
-                                fontFamily: 'irs')),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
+                );
+              },
             ),
-          ),
-        );
-      },
     );
+  }
+
+  void fetchItems() async {
+    Response response = await get(Uri.parse('http://sajjadabbasi.freehost.io/Divar/get_Post.php'));
+
+    var productJson = json.decode(utf8.decode(response.bodyBytes));
+
+    setState(() {
+      for (var tmp in productJson) {
+        var product = AdModel(
+            tmp['title'],
+            tmp['image_link'],
+            tmp['address'],
+            tmp['price'],
+            tmp['count'],
+            tmp['category'],
+            tmp['call_info'],
+            tmp['description'],
+            tmp['status'],
+            tmp['user']);
+        listAds.add(product);
+      }
+
+      debugPrint("listSize: ${listAds.length}");
+    });
   }
 }
